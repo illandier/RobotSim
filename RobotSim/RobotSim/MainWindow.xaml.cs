af98c4dd08a87ccf1a1166be587e3519b52bf9f5
illandier <rafalszypulewski@o2.pl>
2014-02-05 04:39:14 -0800

    main window changes

--- RobotSim/RobotSim/MainWindow.xaml.cs
+++ RobotSim/RobotSim/MainWindow.xaml.cs
@@ -23,6 +23,53 @@ namespace RobotSim
         public MainWindow()
         {
             InitializeComponent();
+            GridSize = 50;
+            SetWidth = int.Parse(MapSetWidth.Text);
+            SetHeight = int.Parse(MapSetHeight.Text);
+            CanvasMainWindow.Width = SetWidth * GridSize;
+            CanvasMainWindow.Height = SetHeight * GridSize;
+            DrawCanvasGrid();
+        }
+        public int GridSize { get; set; }
+        public int SetWidth { get; set; }
+        public int SetHeight { get; set; }
+
+        void DrawCanvasGrid()
+        {
+            SetWidth = int.Parse(MapSetWidth.Text);
+            SetHeight = int.Parse(MapSetHeight.Text);
+            CanvasMainWindow.Width = SetWidth * GridSize;
+            CanvasMainWindow.Height = SetHeight * GridSize;
+            CanvasMainWindow.Children.Clear();
+            double width = CanvasMainWindow.Width;
+            double height = CanvasMainWindow.Height;
+
+            for (int i = 0; i < SetWidth; i++)
+            {
+                for (int j = 0; j < SetHeight; j++)
+                {
+                    Rectangle rec = new Rectangle();
+                    rec.Stroke = Brushes.DarkGray;
+                    rec.StrokeThickness = 1;
+                    rec.Width = GridSize;
+                    rec.Height = GridSize;
+                    Canvas.SetTop(rec, (double)(j * GridSize));
+                    Canvas.SetLeft(rec, (double)(i * GridSize));
+                    CanvasMainWindow.Children.Add(rec);
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawCanvasGrid();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var CreatorWindow = new MapCreator();
+            CreatorWindow.Show();
         }
     }
 }
